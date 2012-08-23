e9036f60f039d919c43bad6632b02a1f8e784293
Mike Eremin <meremin@gmail.com>
2012-08-24 00:23:58 +0400

    some fixes

--- html/cron.py
+++ html/cron.py
@@ -2,14 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management import setup_environ
-import settings, time
-setup_environ(settings)
-from settings import *
+import live_settings, time
+setup_environ(live_settings)
+from live_settings import *
 import datetime, os
 from my.models import Sync
 from django.db.models import Q
 
 nextupdate = datetime.datetime.now() - datetime.timedelta(minutes=30)
+fails = datetime.datetime.now() - datetime.timedelta(hours=10)
 try:
     sites = Sync.objects.filter(Q(last_sync__lte=nextupdate, updating=0)|Q(last_sync=None, updating=0))
     for site in sites:
